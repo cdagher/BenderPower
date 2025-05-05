cbbb386bbfc75141a8ba0da75e98e985feb166d9
qeftser <qeftser1@gmail.com>
2025-05-05 10:03:05 +0900

    configuring cmake

---
+++ firmware/RP2040/lib/ADS1015/src/ads1015.h
@@ -0,0 +1,6 @@
+
+#ifndef __RP2040_FIRMWARE_LIB_ADS1015__
+
+#define __RP2040_FIRMWARE_LIB_ADS1015__
+
+#endif
---
+++ firmware/RP2040/src/main.cpp
@@ -0,0 +1,12 @@
+
+#include "pico/stdlib.h"
+
+int main() {
+
+   while (1) {
+
+      sleep_ms(10000);
+
+   }
+
+}
