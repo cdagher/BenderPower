815a8469ae912158bc311cc064b7f84f02e8f3f2
cdagher <chrisdagher642@gmail.com>
2025-05-11 11:26:43 -0600

    Add device addresses

--- firmware/RP2040/src/board.h
+++ firmware/RP2040/src/board.h
@@ -36,8 +36,8 @@ from the LED driver to the RP2040.
 #define LED_CLK_MODE GPIO_FUNC_SPI
 #define LED_SDI 3
 #define LED_SDI_MODE GPIO_FUNC_SPI
-#define LED_EN  6
-#define LED_EN_MODE GPIO_FUNC_SIO
+#define LED_LEN  6
+#define LED_LEN_MODE GPIO_FUNC_SIO
 
 /* Battery Inputs 
 Batteries are controlled using LM5066I ICs from Texas Instruments.
@@ -45,6 +45,7 @@ Batteries are controlled using LM5066I ICs from Texas Instruments.
 Batteries should use I2C0 for communication with the RP2040.
 */
 
+#define BATTERY_1_ADDR 0x40
 #define BATTERY_1_EN 12
 #define BATTERY_1_EN_PIN_MODE GPIO_FUNC_SIO
 #define BATTERY_1_ALERT 13
@@ -52,6 +53,7 @@ Batteries should use I2C0 for communication with the RP2040.
 #define BATTERY_1_PGD 11
 #define BATTERY_1_PGD_PIN_MODE GPIO_FUNC_SIO
 
+#define BATTERY_2_ADDR 0x41
 #define BATTERY_2_EN 10
 #define BATTERY_2_EN_PIN_MODE GPIO_FUNC_SIO
 #define BATTERY_2_ALERT 14
@@ -59,6 +61,7 @@ Batteries should use I2C0 for communication with the RP2040.
 #define BATTERY_2_PGD 9
 #define BATTERY_2_PGD_PIN_MODE GPIO_FUNC_SIO
 
+#define BATTERY_3_ADDR 0x42
 #define BATTERY_3_EN 8
 #define BATTERY_3_EN_PIN_MODE GPIO_FUNC_SIO
 #define BATTERY_3_ALERT 15
@@ -90,18 +93,24 @@ poll the auxiliary outputs.
 Auxiliary outputs should use I2C1 for communication with the RP2040.
 */
 
+#define AUX_DUMMY_ALERT 16
+#define AUX_DUMMY_ALERT_PIN_MODE GPIO_FUNC_SIO
+
+#define AUX_1_ADDR 0x43
 #define AUX_1_EN 21
 #define AUX_1_EN_PIN_MODE GPIO_FUNC_SIO
 // #define AUX_1_ALERT 20
 #define AUX_1_PGD 20
 #define AUX_1_PGD_PIN_MODE GPIO_FUNC_SIO
 
+#define AUX_2_ADDR 0x44
 #define AUX_2_EN 23
 #define AUX_2_EN_PIN_MODE GPIO_FUNC_SIO
 // #define AUX_2_ALERT 22
 #define AUX_2_PGD 22
 #define AUX_2_PGD_PIN_MODE GPIO_FUNC_SIO
 
+#define AUX_3_ADDR 0x45
 #define AUX_3_EN 25
 #define AUX_3_EN_PIN_MODE GPIO_FUNC_SIO
 // #define AUX_3_ALERT 24
